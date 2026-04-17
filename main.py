5a21cba2e93763b6681c43824629b1efc755a525
Alexandre Boucher <albou631@ulaval.ca>
2026-04-16 22:29:47 -0400

    c) mini fix

--- main.py
+++ main.py
@@ -10,3 +10,23 @@ def f(t, y, dy):
 
 coef = splines_edo_implicite(np.sqrt(2)/2 * np.exp(np.pi/4), np.sqrt(2) * np.exp(np.pi/4), f, np.pi/4, 3, 16)
 
+# spline
+def S(t):
+    h = (3 - np.pi/4)/16
+    i = min(int((t - np.pi/4)//h), 15)
+    a, b, c, d = coef[i]
+    return a*t**3 + b*t**2 + c*t + d
+
+# on crée les intervalles de valeurs:
+t = np.linspace(np.pi/4, 3, 16*20) #20 points en plus par intervalle pour la clarté
+S_pasfonction = np.array([S(t) for t in t]) # on "sincronise" S et t 
+y_exacte = np.exp(t)*np.sin(t)
+
+plt.figure()
+plt.plot(t, y_exacte, label = "solution exacte")
+plt.plot(t, S_pasfonction, label = "Spline cubique")
+plt.legend()
+plt.title('Solutions exacte vs spline cubique')
+plt.xlabel("t")
+plt.ylabel("y(t)")
+plt.show()
