5ba8f6df24ba20706d734de6fc255c78409885bf
Alexandre Boucher <albou631@ulaval.ca>
2026-04-16 21:59:40 -0400

    c) part 1

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from splines_edo_implicite import splines_edo_implicite
 from rk4 import rk4
+import math
+
+# c)
+def f(t, y, dy):
+    return 2*np.exp(-t)*np.sin(t)*y*dy - 2*np.exp(t)*np.sin(t)
+
+coef = splines_edo_implicite(np.sqrt(2)/2 * np.exp(np.pi/4), np.sqrt(2) * np.exp(np.pi/4), f, np.pi/4, 3, 16)
+
