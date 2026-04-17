626a3301c68f471e24af2a88a697d56f5bc2e1a2
Alexandre Boucher <albou631@ulaval.ca>
2026-04-16 22:53:30 -0400

    c) final version

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import math
 
 # c)
 def f(t, y, dy):
-    return 2*np.exp(-t)*np.sin(t)*y*dy - 2*np.exp(t)*np.sin(t)
+    return 2*np.exp(-t)/np.sin(t)*y*dy - 2*np.exp(t)*np.sin(t)
 
 coef = splines_edo_implicite(np.sqrt(2)/2 * np.exp(np.pi/4), np.sqrt(2) * np.exp(np.pi/4), f, np.pi/4, 3, 16)
+
 
 # spline
 def S(t):
@@ -18,13 +19,13 @@ def S(t):
     return a*t**3 + b*t**2 + c*t + d
 
 # on crée les intervalles de valeurs:
-t = np.linspace(np.pi/4, 3, 16*20) #20 points en plus par intervalle pour la clarté
-S_pasfonction = np.array([S(t) for t in t]) # on "sincronise" S et t 
-y_exacte = np.exp(t)*np.sin(t)
+t_int = np.linspace(np.pi/4, 3, 16*20) #20 points en plus par intervalle pour la clarté
+S_pasfonction = np.array([S(t) for t in t_int]) # on "sincronise" S et t 
+y_exacte = np.exp(t_int)*np.sin(t_int)
 
 plt.figure()
-plt.plot(t, y_exacte, label = "solution exacte")
-plt.plot(t, S_pasfonction, label = "Spline cubique")
+plt.plot(t_int, y_exacte, label = "solution exacte")
+plt.plot(t_int, S_pasfonction, label = "Spline cubique")
 plt.legend()
 plt.title('Solutions exacte vs spline cubique')
 plt.xlabel("t")
